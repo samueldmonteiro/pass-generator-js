a07a3444b0b99836f384c1f80184b85492a859d8
Samuel Monteiro <samuel.dvmonteiro@gmail.com>
2022-10-11 15:28:49 -0300

    refactoring JS code organization

--- assets/js/passwordGenerator.js
+++ assets/js/passwordGenerator.js
@@ -1,6 +1,4 @@
-import { qs, qsAll} from "./utils.js";
-
-const characterTypes = {
+export const characterTypes = {
 
     lowercase: "abcefghijklmnoprstuvwxyz",
     uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -8,106 +6,29 @@ const characterTypes = {
     specialCaracters: "!@#$%*_><.;"
 }
 
-const lengthControl = qs("#lengthControl");
-const passwordOptions = qsAll(".options-control .options .option input");
-const passwordElement = qs("#password");
-const copyPasswordElement = qs(".copy-password");
+export const passwordGenerator = {
 
-if (passwordOptions) {
-    passwordOptions.forEach(option => {
-        option.addEventListener("click", showPassword)
-    });
-}
+    generate(length, chars = null) {
 
-if (lengthControl) {
-    lengthControl.addEventListener("input", (e) => {
+        if (chars === null) {
+            chars = "";
+            let keys = Object.keys(characterTypes);
+            keys.forEach((key, index) => {
 
-        qs(".length-control #lengthValue").innerText = e.currentTarget.value;
-        showPassword();
-    })
-}
-
-if (qs(".container-password")) {
-
-    let containerPassword = qs(".container-password");
-
-    containerPassword.addEventListener("mouseover", () => {
-        copyPasswordElement.style.opacity = "1";
-    });
-    containerPassword.addEventListener("mouseout", () => {
-        copyPasswordElement.style.opacity = "0";
-    });
-}
-
-if(copyPasswordElement){
-    copyPasswordElement.addEventListener("click", (e)=>{
-
-        let element = e.currentTarget;
-        element.querySelector("span").innerText = "Senha Copiada";
-
-        let icon = element.querySelector(".bi");
-        icon.classList.remove("bi-clipboard-fill");
-        icon.classList.add("bi-clipboard-check-fill");
-
-        copyPassword();
-    });
-}
-
-function copyPassword(){
-    let password = passwordElement.innerText;
-    navigator.clipboard.writeText(password);
-}
-
-function showPassword() {
-
-    restartWindow();
-
-    let passwordLength = lengthControl.value;
-    let chars = getCharacters();
-    let password = generatePassword(chars, passwordLength);
-
-    if (!password) {
-        passwordElement.innerText = "Sem opções para gerar a senha!";
-        return;
-    }
-    passwordElement.innerText = password;
-}
-
-function generatePassword(chars, length) {
-
-    let password = "";
-
-    for (let i = 0; i < length; i++) {
-        let rand = chars.charAt(Math.floor(Math.random() * chars.length));
-        password += rand;
-    }
-    return password;
-}
-
-function getCharacters() {
-
-    let chars = "";
-
-    passwordOptions.forEach(option => {
-        if (option.checked) {
-            let type = option.id;
-            chars += characterTypes[type];
+                let type = keys[Math.ceil(Math.random() * index)];
+                chars += characterTypes[type];
+            });
         }
-    });
-    return chars;
-}
 
-function restartWindow(){
+        let password = "";
+        for (let i = 0; i < length; i++) {
+            let rand = chars.charAt(Math.floor(Math.random() * chars.length));
+            password += rand;
+        }
+        return password;
+    },
 
-    let copyIcon = copyPasswordElement.querySelector(".bi");
-    copyIcon.classList.add("bi-clipboard-fill");
-    copyIcon.classList.remove("bi-clipboard-check-fill");
-    copyPasswordElement.querySelector("span").innerText = "Copiar Senha";
-}
-
-function init() {
-    qs(".length-control #lengthValue").innerText = lengthControl.value;
-    showPassword();
-}
-
-init();
+    copyPassword(password) {
+        navigator.clipboard.writeText(password);
+    },
+};
