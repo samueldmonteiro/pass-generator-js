e234e3990a7cec7ac9781ebb4f39b7003ad3f2e3
Samuel Monteiro <samuel.dvmonteiro@gmail.com>
2022-10-11 15:14:37 -0300

    refactoring JS code organization

---
+++ assets/js/app.js
@@ -0,0 +1,86 @@
+import { qs, qsAll } from "./modules/utils.js";
+import { passwordGenerator, characterTypes } from "./modules/passwordGenerator.js";
+
+const passwordLengthControl = qs("#lengthControl");
+const passwordSettings = qsAll(".options-control .options .option input");
+const passwordElement = qs("#password");
+const btnCopyPassword = qs(".copy-password");
+
+if (passwordSettings) {
+    passwordSettings.forEach(setting => {
+        setting.addEventListener("click", _ => {
+            showPassword();
+        })
+    });
+}
+
+if (passwordLengthControl) {
+    passwordLengthControl.addEventListener("input", e => {
+        qs(".length-control #lengthValue").innerText = e.currentTarget.value;
+        showPassword();
+    })
+}
+
+if (btnCopyPassword) {
+
+    qs(".container-password").addEventListener("mouseover", () => {
+        btnCopyPassword.style.opacity = "1";
+    });
+    qs(".container-password").addEventListener("mouseout", () => {
+        btnCopyPassword.style.opacity = "0";
+    });
+
+    btnCopyPassword.addEventListener("click", (e) => {
+
+        const btn = e.currentTarget;
+        btn.querySelector("span").innerText = "Senha Copiada";
+
+        const icon = btn.querySelector(".bi");
+        icon.classList.remove("bi-clipboard-fill");
+        icon.classList.add("bi-clipboard-check-fill");
+
+        passwordGenerator.copyPassword(passwordElement.innerText);
+    });
+}
+
+const getCharacters = () => {
+    let chars = "";
+    passwordSettings.forEach(setting => {
+        if (setting.checked) {
+            let type = setting.id;
+            chars += characterTypes[type] || "";
+        }
+    });
+    return chars;
+}
+
+const showPassword = () => {
+
+    restartWindow();
+
+    const passwordLength = passwordLengthControl.value;
+    const chars = getCharacters();
+    const password = passwordGenerator.generate(passwordLength, chars);
+
+    if (!password) {
+        passwordElement.innerText = "Sem opções para gerar a senha!";
+        return;
+    }
+    passwordElement.innerText = password;
+}
+
+const restartWindow = () => {
+
+    let copyIcon = btnCopyPassword.querySelector(".bi");
+    copyIcon.classList.add("bi-clipboard-fill");
+    copyIcon.classList.remove("bi-clipboard-check-fill");
+    btnCopyPassword.querySelector("span").innerText = "Copiar Senha";
+}
+
+const init = () => {
+    qs(".length-control #lengthValue").innerText = passwordLengthControl.value;
+    showPassword();
+}
+
+init();
+
---
+++ assets/js/modules/passwordGenerator.js
@@ -0,0 +1,34 @@
+export const characterTypes = {
+
+    lowercase: "abcefghijklmnoprstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    specialCaracters: "!@#$%*_><.;"
+}
+
+export const passwordGenerator = {
+
+    generate(length, chars = null) {
+
+        if (chars === null) {
+            chars = "";
+            let keys = Object.keys(characterTypes);
+            keys.forEach((key, index) => {
+
+                let type = keys[Math.ceil(Math.random() * index)];
+                chars += characterTypes[type];
+            });
+        }
+
+        let password = "";
+        for (let i = 0; i < length; i++) {
+            let rand = chars.charAt(Math.floor(Math.random() * chars.length));
+            password += rand;
+        }
+        return password;
+    },
+
+    copyPassword(password) {
+        navigator.clipboard.writeText(password);
+    },
+};
